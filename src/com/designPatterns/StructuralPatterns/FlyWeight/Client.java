d68e1bf7b2747e5dfc13e6a689668eafee4ca26a
abbas <62177195+Omar-ahmed314@users.noreply.github.com>
2023-02-25 12:28:58 +0200

    create flyweight pattern

---
+++ src/com/designPatterns/StructuralPatterns/FlyWeight/Client.java
@@ -0,0 +1,9 @@
+package com.designPatterns.StructuralPatterns.FlyWeight;
+
+public class Client {
+    public static void main(String[] args) {
+        // some code will be written here
+        // code
+
+    }
+}
